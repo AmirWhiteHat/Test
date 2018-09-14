7d78ebc4bac184d1e7838eca4ffaa363b356cddb
Nima HeydariNasab <nima.heydari79@yahoo.com>
2018-09-14 20:39:22 +0000

    Apply fixes from StyleCI

--- bot.php
+++ bot.php
@@ -51,14 +51,12 @@ while (true) {
                         $text = $message['text'];
                         if ($text == '/start') {
                             apiRequest('sendMessage', ['chat_id' => $chat_id, 'text' => "Welcome to @AvailabilityCheckerBot!\n\nThis bot can check availability of domain names in different TLDs.\n\nSend me a domain name to check availability.\n\nMade with ❤ by @Radio_Nima", 'reply_to_message_id' => $message_id]);
-                        }
-                        elseif (preg_match('/^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$/', $text)) {
+                        } elseif (preg_match('/^([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*$/', $text)) {
                             $sent = apiRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Checking availability of domain name...', 'reply_to_message_id' => $message_id]);
                             $sent = json_decode($sent, true);
                             $sent = $sent['result']['message_id'];
                             apiRequest('editMessageText', ['chat_id' => $chat_id, 'message_id' => $sent, 'text' => $text.'.ir: '.isDomainAvailable($text.'.ir')."\n".$text.'.com: '.isDomainAvailable($text.'.com')."\n".$text.'.net: '.isDomainAvailable($text.'.net')."\n".$text.'.org: '.isDomainAvailable($text.'.org')."\n".$text.'.info: '.isDomainAvailable($text.'.info')."\n".$text.'.co: '.isDomainAvailable($text.'.co')."\n".$text.'.biz: '.isDomainAvailable($text.'.biz')."\n".$text.'.xyz: '.isDomainAvailable($text.'.xyz')."\n".$text.'.tk: '.isDomainAvailable($text.'.tk')]);
-                        }
-                        else {
+                        } else {
                             apiRequest('sendMessage', ['chat_id' => $chat_id, 'text' => 'Invalid domain name!', 'reply_to_message_id' => $message_id]);
                         }
                     }
